f0eb2a5a049780acc0784a187005259f97f414a2
knowbee <knowbeeinc@gmail.com>
2020-12-24 19:49:21 +0200

    config module

---
+++ config/__init__.py
@@ -0,0 +1,7 @@
+# Go ahead and update this
+email = ""
+password = ""
+
+# Don't change this, unless you know what you are doing :)
+login_url = "https://www.linkedin.com/learning-login"
+main_url = "https://www.linkedin.com/learning/"
