94864d91e531a84241a6867136119d16aea05c61
knowbee <knowbeeinc@gmail.com>
2020-12-28 10:46:31 +0200

    update config file

--- llvd/config.py
+++ llvd/config.py
@@ -1,4 +1,5 @@
 # Don't change this, unless you know what you are doing :)
-login_url = "https://www.linkedin.com/learning-login"
+login_url = "https://www.linkedin.com/checkpoint/lg/login"
+signup_url = "https://www.linkedin.com/checkpoint/lg/login-submit"
 course_url = "https://www.linkedin.com/learning-api/detailedCourses??fields=videos&addParagraphsToTranscript=true&courseSlug={0}&q=slugs"
 video_url = "https://www.linkedin.com/learning-api/detailedCourses?addParagraphsToTranscript=false&courseSlug={0}&q=slugs&resolution=_360&videoSlug={1}"
