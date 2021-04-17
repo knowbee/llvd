f7e143c9c7777c41186de0ce34005e8f8bf32256
knowbee <knowbeeinc@gmail.com>
2021-04-17 16:41:26 +0200

    2.0.8

--- llvd/__init__.py
+++ llvd/__init__.py
@@ -35,7 +35,7 @@ def main(cookies, course, resolution, caption):
 
     course = course.lower().replace("c#", "c-sharp").replace(".net", "dot-net")
     without_chars = re.sub(r'[\':)(,>.’/]', " ", course.strip()).replace("«", " ").replace("-»", " ").replace("»", " ").strip()
-    course_slug= re.sub(r'(\s+)', "", without_chars.lower()))
+    course_slug= re.sub(r'(\s+)', "-", without_chars.lower())
     email = ""
     password = ""
     if cookies:
