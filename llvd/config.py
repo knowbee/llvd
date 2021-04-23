4f3864039f8932af961607d1899f9ea64e023cb2
João Loureiro <joaofl@gmail.com>
2021-04-23 13:04:09 +0200

    Add user/pass defaults for better automation

--- llvd/config.py
+++ llvd/config.py
@@ -3,3 +3,6 @@ login_url = "https://www.linkedin.com/checkpoint/lg/login"
 signup_url = "https://www.linkedin.com/checkpoint/lg/login-submit"
 course_url = "https://www.linkedin.com/learning-api/detailedCourses??fields=videos&addParagraphsToTranscript=true&courseSlug={0}&q=slugs"
 video_url = "https://www.linkedin.com/learning-api/detailedCourses?addParagraphsToTranscript=false&courseSlug={0}&q=slugs&resolution=_{1}&videoSlug={2}"
+
+email=""
+password=""
